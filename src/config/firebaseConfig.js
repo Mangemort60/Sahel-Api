0ad513bedc2dbcf40337950caf12ce9868ae96a7
Hafid HADDAOUI <hahaddaoui@gmail.com>
2024-06-15 21:09:44 +0200

    Update start script to use node instead of nodemon

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,10 +1,23 @@
-
 // firebase.js
+require('dotenv').config();
 const admin = require("firebase-admin")
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore, FieldValue } = require('firebase-admin/firestore'); // getFirestore est une instance de firestore, et fielValue une fonction qui permet d'agir sur les champs
 const { getStorage } = require('firebase-admin/storage');
-const serviceAccount = require('../../sahel-26e16-firebase-adminsdk-jx0y3-42c287a958.json');
+
+
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL
+};
 
 admin.initializeApp({
   credential: cert(serviceAccount),

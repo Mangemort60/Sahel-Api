b8f8a5cd00d47c93b89a74ed25f2b60d2fca73e1
Hafid HADDAOUI <hahaddaoui@gmail.com>
2024-06-15 21:40:05 +0200

    edited firebasConfig

--- functions/firebaseFunctionsConfig.js
+++ functions/firebaseFunctionsConfig.js
@@ -1,12 +1,13 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable max-len */
+require("dotenv").config();
 const admin = require("firebase-admin");
 
 const serviceAccount = {
   type: "service_account",
   project_id: process.env.FIREBASE_PROJECT_ID,
   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  private_key: JSON.parse(process.env.FIREBASE_PRIVATE_KEY),
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
   client_id: process.env.FIREBASE_CLIENT_ID,
   auth_uri: process.env.FIREBASE_AUTH_URI,

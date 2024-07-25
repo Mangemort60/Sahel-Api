180a89521b6aad08b75fee24aa56f733d45f3d35
Hafid HADDAOUI <hahaddaoui@gmail.com>
2024-07-25 19:07:50 +0200

    edited firebase function

--- functions/firebaseFunctionsConfig.js
+++ functions/firebaseFunctionsConfig.js
@@ -3,11 +3,13 @@
 require("dotenv").config();
 const admin = require("firebase-admin");
 
+console.log("MY_PRIVATE_KEY:", process.env.MY_PRIVATE_KEY ? "PRIVATE_KEY_EXISTS" : "NO_PRIVATE_KEY");
+
 const serviceAccount = {
   type: "service_account",
   project_id: process.env.MY_FIREBASE_PROJECT_ID,
   private_key_id: process.env.MY_FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.MY_FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n") : undefined,
+  private_key: process.env.MY_FIREBASE_PRIVATE_KEY ? process.env.MY_FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n") : undefined,
   client_email: process.env.MY_FIREBASE_CLIENT_EMAIL,
   client_id: process.env.MY_FIREBASE_CLIENT_ID,
   auth_uri: process.env.MY_FIREBASE_AUTH_URI,

49360ed3b798e6a6151e8906192f4e2fbacbaa0a
Hafid HADDAOUI <hahaddaoui@gmail.com>
2024-10-02 17:24:44 +0200

    Fix CORS issues

--- src/app.js
+++ src/app.js
@@ -1,34 +1,53 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 const app = express();
-const authRoutes = require('./api/routes/authRoutes');
-const quoteRoutes = require('./api/routes/quoteRoutes');
-const paymentRoutes = require('./api/routes/paymentRoutes')
-const reservationRoutes = require('./api/routes/reservationRoutes');
-const chatRoutes = require('./api/routes/chatRoutes')
-const contactRoutes = require('./api/routes/contactRoutes')
-const helmet = require('helmet');
+const authRoutes = require("./api/routes/authRoutes");
+const quoteRoutes = require("./api/routes/quoteRoutes");
+const paymentRoutes = require("./api/routes/paymentRoutes");
+const reservationRoutes = require("./api/routes/reservationRoutes");
+const chatRoutes = require("./api/routes/chatRoutes");
+const contactRoutes = require("./api/routes/contactRoutes");
+const helmet = require("helmet");
 
-app.use(helmet()); 
+app.use(helmet());
 app.use(express.json());
-app.use(cors({
-  origin: ['http://localhost:4173','http://localhost:5173','http://localhost:3000', 'https://sahel-back-286afc7b2555.herokuapp.com', 'https://sahel-26e16.web.app', 'https://sahel-admin.web.app' ,'https://sahel-26e16.firebaseapp.com', "https://sahel-admin.firebaseapp.com" ], // Ajoutez vos domaines frontend ici
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:4173",
+      "http://localhost:5173",
+      "http://localhost:3000",
+      "https://sahel-back-286afc7b2555.herokuapp.com",
+      "https://sahel-26e16.web.app",
+      "https://sahel-admin.web.app",
+      "https://sahel-26e16.firebaseapp.com",
+      "https://sahel-admin.firebaseapp.com",
+    ], // Ajoutez vos domaines frontend ici
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.options("*", (req, res) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.send();
 });
 
-app.use('/auth', authRoutes)
-app.use('/', quoteRoutes)
-app.use('/', reservationRoutes)
-app.use('/', paymentRoutes)
-app.use('/auth', authRoutes)
-app.use('/', chatRoutes)
-app.use('/', contactRoutes)
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
+app.use("/auth", authRoutes);
+app.use("/", quoteRoutes);
+app.use("/", reservationRoutes);
+app.use("/", paymentRoutes);
+app.use("/auth", authRoutes);
+app.use("/", chatRoutes);
+app.use("/", contactRoutes);
 
-module.exports = app
+module.exports = app;
